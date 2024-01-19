2f9e9b94dc4ed186052bde0a009a9c403c31267c
sheetedSquid <dmaltsev6@gmail.com>
2024-01-19 19:16:47 +0200

    connect to mongoDB

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <Link to='/'>
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                     <span className='text-slate-500'>Real</span>
-                    <span className='text-slate-700'>EstateSales</span>
+                    <span className='text-slate-700'>CarSales</span>
                 </h1>
             </Link>
             <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
